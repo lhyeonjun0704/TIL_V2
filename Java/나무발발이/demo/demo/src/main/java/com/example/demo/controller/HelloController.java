84612a660325919bf2289a5ff236335828436d79
lhyeonjun0704 <66072431+lhyeonjun0704@users.noreply.github.com>
2021-08-22 21:48:03 +0900

    cpp_0822

---
+++ Java/나무발발이/demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello") // get 방식의 url post 이다.
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; // 이 모델을 hello라는 페이지로 렌더링 해라는 뜻이다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body부분에 이 데이터를 직접 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    // json의 방식이다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        //java bin 규약?
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
+}
--- c++/Study1/Study01/Study01/cpp_ex13.cpp
+++ c++/Study1/Study01/Study01/cpp_ex13.cpp
@@ -1,11 +1,35 @@
 // 임시 객체는 r-value다!
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Item {
+public:
+	Item(int num) :num(num) { // 변환 생성자
+		cout << "Item(int)" << endl;
+	}
+
+	Item(string name) : name(name) {
+		cout << "Item(string)" << endl;
+	}
+
+	Item(int num, string name) : num(num), name(name) {
+		cout << "Item(int, string)" << endl;
+	}
+
+private:
+	int num;
+	string name;
+};
+
 int main() {
 
-
+	Item i1 = Item(1);
+	Item i2(2);
+	Item i3 = 3;
+	i3 = 3; // 묵시적 형변환
+	Item i4 = (Item)4; // 명시적 형변환
 
 }
