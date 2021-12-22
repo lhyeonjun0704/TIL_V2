0ce1a4b432c90fefff76f4cea8af670a98d3a759
Hyonjun Lee <hello.git.toqur7000@naver.com>
2021-12-22 21:16:06 +0900

    algo_211222

---
+++ Java/코딩테스트_java/Backjoon/question2/src/question7_5/Main.java
@@ -0,0 +1,49 @@
+package question8_5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int n, m, answer = 0;
+	static int[] dis;
+	int[] ch;
+	Queue<Integer> Q = new LinkedList<>();
+	
+	public int BFS(int n, int m) {
+		ch = new int[1000];
+		ch[n] = 1;
+		Q.offer(n);
+		int L = 0;
+		while(!Q.isEmpty()) {
+			int len = Q.size();
+			for(int i = 0; i < len; i++) {
+				int x = Q.poll();
+				for(int j = 0; j < dis.length; j++) {
+					int nx = x + dis[j];
+					if(nx == m) return L+1;
+					if(nx >= 1 && ch[nx] == 0) {
+						ch[nx] = 1;
+						Q.offer(nx);
+					}
+				}
+			}
+			L++;
+		}
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		dis = new int[n];
+		for(int i = 0; i < n; i++) {
+			dis[i] = sc.nextInt();
+		}
+		m = sc.nextInt();
+		System.out.println(T.BFS(0, m));
+	}
+}
---
+++ Java/코딩테스트_java/Backjoon/question2/src/question7_6/Main.java
@@ -0,0 +1,41 @@
+package question8_6;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static int[] pm, ch, arr;
+	static int n, m;
+	public void DFS(int L) {
+		if(L == m) {
+			for(int x : pm) {
+				System.out.print(x + " ");
+			}
+			System.out.println();
+			return;
+		} else {
+			for(int i = 0; i < n; i++) {
+				if(ch[i] == 0) {
+					ch[i] = 1;
+					pm[L] = arr[i];
+					DFS(L+1);
+					ch[i] = 0;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n];
+		for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		ch = new int[n];
+		pm = new int[m];
+		T.DFS(0);
+	}
+
+}
